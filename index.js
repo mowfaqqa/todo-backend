7767221665b7aaf6e359bc067156b3e4ef2fa953
muwaffaq <itzmuwaffaq@gmail.com>
2023-03-23 03:40:19 +0100

    fixes

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const PORT = 5000
-const todos = require('./routes/todoRoutes')
-const connectDB = require('./db/connect')
-require('dotenv').config()
+const app = express();
+const PORT = process.env.PORT || 5000;
+const todos = require("./routes/todoRoutes");
+const connectDB = require("./db/connect");
+const { getAllTodos } = require("./controllers/todosController");
+require("dotenv").config();
 // MIDDLEWARE
-app.use(express.json())
+app.use(express.json());
 
-// ROUTES 
-app.use('/api', todos)
+// ROUTES
+app.get("/", getAllTodos);
+
+app.use("/api", todos);
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(PORT, () => {
-          console.log(`API listening on PORT ${PORT} `)
-        })
- } catch (error) {
-    console.log(error)
- }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`API listening on PORT ${PORT} `);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // CALLING FUNCTION TO LISTEN TO SERVER
 start();
 
-
 // Export the Express API
-module.exports = app 
+module.exports = app;
